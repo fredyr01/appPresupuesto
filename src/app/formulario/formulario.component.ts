483f035f7826ce4643c6f972815ffa2b8c7c0bf6
fredyr01 <fredyrios01@gmail.com>
2021-09-28 11:49:30 -0500

    Finalizado el ejercicio, se añade la interacción con el formulario, uso de ngclass, pipes, validación

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Egreso } from '../egreso/egreso.modelo';
+import { EgresoServicio } from '../egreso/egreso.servicio';
+import { Ingreso } from '../ingreso/ingreso.modelo';
+import { IngresoServicio } from '../ingreso/ingreso.servicio';
 
 @Component({
   selector: 'app-formulario',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formulario.component.css']
 })
 export class FormularioComponent implements OnInit {
+  tipo:string="ingresoOperacion";
+  descripcionInput:string;
+  valorInput:number;
 
-  constructor() { }
+  constructor(private ingresoServicio: IngresoServicio,
+              private egresoServicio: EgresoServicio) { }
 
   ngOnInit(): void {
+  }
+
+  tipoOperacion(evento:any){
+    this.tipo = evento.target.value;   // * Se accede al valor del component select
+  }
+
+  agregarValor(){
+    if(this.tipo === "ingresoOperacion")
+    this.ingresoServicio.ingresos.push(new Ingreso(this.descripcionInput, this.valorInput));
+    else
+    this.egresoServicio.egresos.push(new Egreso(this.descripcionInput, this.valorInput));
   }
 
 }
